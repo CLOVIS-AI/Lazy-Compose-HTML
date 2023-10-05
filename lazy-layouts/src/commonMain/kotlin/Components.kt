9def6561ec53308a231c9f01dd404dcaf2e68b04
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-10-05 21:22:06 +0200

    feat(lazy-layouts): Create the LazyColumn and LazyRow components

--- lazy-layouts/src/commonMain/kotlin/Components.kt
+++ lazy-layouts/src/commonMain/kotlin/Components.kt
@@ -1,1 +1,81 @@
 package opensavvy.compose.lazy
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import org.jetbrains.compose.web.css.DisplayStyle
+import org.jetbrains.compose.web.css.FlexDirection
+import org.jetbrains.compose.web.css.display
+import org.jetbrains.compose.web.css.flexDirection
+import org.jetbrains.compose.web.dom.AttrBuilderContext
+import org.w3c.dom.HTMLDivElement
+
+/**
+ * HTML-based lazy column implementation.
+ *
+ * Use this composable to display large amounts of elements on screen.
+ * Items are only loaded as they appear on screen.
+ *
+ * Elements are loaded from top to bottom, in the same order as they are declared in [block].
+ *
+ * ### Example
+ *
+ * Lazily display a large list of users, unreferencing each from its by `UserId` by calling `requestUser()`:
+ *
+ * ```kotlin
+ * @Composable
+ * fun ShowUsers(users: List<UserId>) {
+ *     LazyColumn {
+ *         items(users) { userId ->
+ *             var user by remember { mutableStateOf<User?>(null) }
+ *
+ *             LaunchedEffect(userId) {
+ *                 user = requestUser(userId)
+ *             }
+ *
+ *             user?.also { Show(it) }
+ *         }
+ *     }
+ * }
+ * ```
+ */
+@Composable
+fun LazyColumn(
+	attrs: AttrBuilderContext<HTMLDivElement>? = null,
+	block: LazyDsl.() -> Unit,
+) {
+	val dsl = remember(block) { LazyDsl().also(block) }
+
+	LazyLinearLayout(dsl) {
+		style {
+			display(DisplayStyle.Flex)
+			flexDirection(FlexDirection.Column)
+		}
+
+		attrs?.invoke(this)
+	}
+}
+
+/**
+ * HTML-based lazy row implementation.
+ *
+ * Use this composable to display large amounts of elements on screen.
+ * Items are only loaded as they appear on screen.
+ *
+ * Elements are loaded from side to side, respecting the user's reading direction, in the same order as they are declared in [block].
+ */
+@Composable
+fun LazyRow(
+	attrs: AttrBuilderContext<HTMLDivElement>? = null,
+	block: LazyDsl.() -> Unit,
+) {
+	val dsl = remember(block) { LazyDsl().also(block) }
+
+	LazyLinearLayout(dsl) {
+		style {
+			display(DisplayStyle.Flex)
+			flexDirection(FlexDirection.Row)
+		}
+
+		attrs?.invoke(this)
+	}
+}
