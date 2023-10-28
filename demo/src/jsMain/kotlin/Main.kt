a178e793aa9bddbe54f4ee7e633923b1ecc52460
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-10-28 20:36:12 +0200

    feat(demo): Add a mutable section

--- demo/src/jsMain/kotlin/Main.kt
+++ demo/src/jsMain/kotlin/Main.kt
@@ -1,13 +1,22 @@
 package opensavvy.compose.lazy.demo
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.Snapshot
 import opensavvy.compose.lazy.LazyColumn
+import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
+import kotlin.random.Random
+import kotlin.random.nextInt
+import kotlin.random.nextUInt
 
 fun main() {
 	val letterIndexes = List(26) { it }
 
 	renderComposable(rootElementId = "root") {
+		val randomElements = remember { mutableStateListOf<UInt>() }
+
 		LazyColumn {
 			item {
 				Text("First element")
@@ -15,6 +24,25 @@ fun main() {
 
 			items(200, key = { it }) {
 				Text("Element #$it")
+			}
+
+			item {
+				Button({
+					onClick {
+						Snapshot.withMutableSnapshot {
+							randomElements.clear()
+							repeat(Random.nextInt(10..100)) {
+								randomElements.add(Random.nextUInt())
+							}
+						}
+					}
+				}) {
+					Text("Generate random elements")
+				}
+			}
+
+			items(randomElements) {
+				Text("Random value: $it")
 			}
 
 			items(letterIndexes) {
